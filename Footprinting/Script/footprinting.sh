2191b3d0a414c1748002c2d06ca4400913a3d488
zetsu <ebahniagoran@gmail.com>
2025-03-21 21:02:10 +0100

    Actualisation du script de footprinting

--- Footprinting/Script/footprinting.sh
+++ Footprinting/Script/footprinting.sh
@@ -6,14 +6,14 @@ if [ "$1" = "" ]; then
     exit 1
 fi
 
-# Récupérer le nom de domaine et créer le dossier
+# Récupérer le nom de domaine 
 DOMAIN=$1
 DIR="footprinting-$DOMAIN"
 mkdir -p "$DIR"
 
 echo "[+] Collecte des informations sur $DOMAIN..."
 
-# Whois - Informations sur le domaine
+# Whois Informations sur le domaine
 whois $DOMAIN > "$DIR/whois.txt"
 echo "[+] Whois terminé."
 
@@ -21,22 +21,22 @@ echo "[+] Whois terminé."
 nslookup $DOMAIN > "$DIR/nslookup.txt"
 echo "[+] Nslookup terminé."
 
-# Dig - Enregistrements DNS
+# Dig Enregistrements DNS
 dig $DOMAIN ANY > "$DIR/dig.txt"
 echo "[+] Dig terminé."
 
-# TheHarvester - Recherche d'informations en ligne
+# TheHarvester  Recherche d'informations en ligne
 theHarvester -d $DOMAIN -l 500 -b all > "$DIR/theHarvester.txt"
 echo "[+] TheHarvester terminé."
 
-# Traceroute - Chemin parcouru par les données
+# Traceroute Chemin parcouru par les données
 traceroute $DOMAIN > "$DIR/traceroute.txt"
 echo "[+] Traceroute terminé."
 
-# Host - Informations sur le serveur
+# Host  Informations sur le serveur
 host -a $DOMAIN > "$DIR/host.txt"
 echo "[+] Host terminé."
 
 # Message final
-echo "[+] Footprinting terminé. Résultats enregistrés dans $DIR."
+echo "Footprinting terminé."
 
